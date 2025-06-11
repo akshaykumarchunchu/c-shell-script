b2fda7ba76d3ada7bffdd923c3f42186970b61df
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-10 21:33:45 -0400

    some message

--- 12-repeat.sh
+++ 12-repeat.sh
@@ -2,30 +2,36 @@
 
 userid=$(id -u)
 
-# Update package list
-echo "Updating package list..."
+if [ $userid -ne 0 ]
+then 
+    echo "Your a super user"
+    exit 1
+else
+    echo "Not Super user"
+fi 
+
+dnf install mysql -y 
+
+echo "Mysql installed successfully"
 
 # Install nginx
-echo "Installing NGINX..."
-
-dnf install nginx -y
-
-if [ $userid -ne 0 ]
-then
-    echo "NGINX installed successfully."
-else
-    echo "Failed to install NGINX."
-    exit 1
-fi
-
-# Install git
-echo "Installing Git..."
 if [ $? -ne 0 ]
 then
-    echo "Git installed successfully."
-else
-    echo "Failed to install Git."
+    echo "NGINX installed successfully."
     exit 1
+else
+    echo "Failed to install NGINX."
+fi
+
+dnf install nginx -y 
+
+if [ $? 0 ]
+then 
+    echo "Git installed Success"
+    exit 1
+else 
+    echo "Failed to installed git"
 fi
 
 dnf install git -y
+
