0e4a83b9391c540e79add1f11c9043acdd8aaaa6
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-10 21:19:43 -0400

    some message

--- 12-repeat.sh
+++ 12-repeat.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+userid=$(id -u)
+
 # Update package list
 echo "Updating package list..."
-sudo apt update
 
 # Install nginx
 echo "Installing NGINX..."
-if sudo apt install -y nginx; then
-    echo "✅ NGINX installed successfully."
+if [ userid -ne 0]; 
+then
+    echo "NGINX installed successfully."
 else
-    echo "❌ Failed to install NGINX."
+    echo "Failed to install NGINX."
     exit 1
 fi
 
 # Install git
 echo "Installing Git..."
-if sudo apt install -y git; then
-    echo "✅ Git installed successfully."
+if [ $? -ne 0 ]; 
+then
+    echo "Git installed successfully."
 else
-    echo "❌ Failed to install Git."
+    echo "Failed to install Git."
     exit 1
 fi
