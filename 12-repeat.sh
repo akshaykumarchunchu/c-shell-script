ef841542e92d7927fea3180d750a5e517a45c0da
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-10 21:53:04 -0400

    some message

--- 12-repeat.sh
+++ 12-repeat.sh
@@ -1,37 +1,6 @@
-#!/bin/bash
+number=$1
 
-userid=$(id -u)
+if [ $number -gt 10 ]
 
-if [ $userid -ne 0 ]
-then 
-    echo "Run with a super user"
-    exit 1
-else
-    echo "Your a Super user"
-fi 
-
-dnf install mysql -y 
-
-echo "Mysql installed successfully"
-
-# Install nginx
-if [ $? -ne 0 ]
-then
-    echo "NGINX installed failed."
-    exit 1
-else
-    echo "Success to install NGINX."
-fi
-
-dnf install nginx -y 
-
-if [ $? -ne 0 ]
-then 
-    echo "Git installed Success"
-    exit 1
-else 
-    echo "Failed to installed git"
-fi
-
-dnf install git -y 
-
+echo "$number is greater in 10"
+echo "$number is less then 10"
