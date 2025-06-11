8f78c866d24e26af2577cfe150c2c73a2118053e
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-10 21:54:35 -0400

    some message

--- 12-repeat.sh
+++ 12-repeat.sh
@@ -1,6 +1,8 @@
 number=$1
 
 if [ $number -gt 10 ]
-
-echo "$number is greater in 10"
-echo "$number is less then 10"
+then 
+    echo "$number is greater in 10"
+else
+    echo "$number is less then 10"
+fi
