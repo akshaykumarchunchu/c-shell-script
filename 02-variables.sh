8ec4746cc50b2d92fb87d12d4637fd2671691b3e
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-11 12:36:30 -0400

    some message

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Ramesh: How are you Suresh"
-echo "Suresh: i'm good Ramesh, How are you?"
-echo "Ramesh: im good too, How is your work?"
-echo "Suresh: not bad. I am thinking to upgrade to Devops"
+echo "\e[31m Ramesh: How are you Suresh"
+echo "\e[32m Suresh: i'm good Ramesh, How are you?"
+echo "\e[33m Ramesh: im good too, How is your work?"
+echo "\e[0m Suresh: not bad. I am thinking to upgrade to Devops"
  
