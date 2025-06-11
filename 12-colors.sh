a863738f4a01108fd5a91f2fda94db410403558e
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-11 16:45:12 -0400

    some message

---
+++ 12-colors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then 
+    echo "$2 is a failure"
+    exit 1
+else    
+    ehco "$2 is a success"
+fi
+}
+
+
+if [ $userid -ne 0 ]
+then
+    echo "Your not a super user"
+else
+    echo "Your a super user"
+fi
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $? "Installing mysql"
+
+dnf install git -y &>>LOGFILE
+VALIDATE $? "Installating git"
+
