7a6ae12615929a6cc8d1650f9b0c8f2d11d82721
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-11 14:17:24 -0400

    some message

---
+++ 11-functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then 
+    echo "Please run with super user"
+    exit 1
+else
+    echo "You're a super user"
+fi
+
+VALIDATE()
+if [ $1 -ne 0 ]
+then 
+    echo "$1 is failure"
+    exit 1
+else    
+    echo "$2 is success"
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installating mysql"
+
+dnf install git -y
+VALIDATE $? "Installating git"
+
+
+
+
+
+
+
+
+
+
+
