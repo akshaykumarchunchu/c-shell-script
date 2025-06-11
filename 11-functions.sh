24601eb9ee0b402114ae63ae0e097b2ccf4fe210
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-11 16:34:38 -0400

    some message

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,11 +5,6 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
 VALIDATE(){
 if [ $1 -ne 0 ]
 then 
@@ -28,11 +23,11 @@ else
     echo "You're a super user"
 fi
 
-dnf install mysql -y
-VALIDATE $? -e "$G Installating mysql"
+dnf install mysql -y @>>$LOGFILE
+VALIDATE $? "Installating mysql"
 
-dnf install git -y
-VALIDATE $? -e "$G Installating git"
+dnf install git -y @>>$LOGFILE
+VALIDATE $? "Installating git"
 
 
 
