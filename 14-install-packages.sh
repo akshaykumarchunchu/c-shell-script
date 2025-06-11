3d8e65e3e606c916c06f02e2c5b78079adc3062c
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-11 17:18:31 -0400

    some message

---
+++ 14-install-packages.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+userid=(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-$S)
+SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is failure"
+    else
+        echo "$2 is success"
+}
+
+if [ $userid -ne 0 ]
+then 
+    echo "Please enter with super user"
+    exit 1
+else    
+    echo "Your a Super user"
+fi
+
+for i in $@
+do 
+    echo "installing $i"
+done
