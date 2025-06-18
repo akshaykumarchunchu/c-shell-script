af296d6119b19f4a6040353bcceaf5c5797a96ca
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-18 15:57:01 -0400

    some message

---
+++ 15-repeat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d '.' -f1)
+LOGFILE=/temp/$SCRIPTNAME-$TIME_STAMP.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $userid ne 0 ]
+    then 
+        echo "you're not a super user"
+        exit 1
+    else    
+        echo "you're a super user"
+    fi
+}
+
+dnf install git -y &>>LOGFILE
+VALIDATE $? "Installed git is Success"
