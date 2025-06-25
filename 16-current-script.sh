b6ca17696525bc40ad3989828f2c7707b1833ca9
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-25 19:22:18 -0400

    some message

--- 16-current-script.sh
+++ 16-current-script.sh
@@ -7,7 +7,7 @@ echo "PROCESS ID of current shell script: $$"
 
 ./17-other-script.sh 
 
-echo "After calling from other script: $COURSE"
+echo "After calling from cureent script: $COURSE"
 echo "Process ID of current shell script: $$"
 
 
