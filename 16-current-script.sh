43c75f45614b83fbc9b07c3c094a99759047941c
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-25 19:34:37 -0400

    some message

--- 16-current-script.sh
+++ 16-current-script.sh
@@ -5,7 +5,7 @@ COURSE="Devops from current script"
 echo "Before calling from other script:$COURSE"
 echo "PROCESS ID of current shell script: $$"
 
-./17-other-script.sh 
+source/17-other-script.sh
 
 echo "After calling from cureent script: $COURSE"
 echo "Process ID of current shell script: $$"
--- 17-other-script.sh
+++ 17-other-script.sh
@@ -2,6 +2,6 @@
 
 COURSE="Devops after calling from other script"
 
-echo "After calling from other script: $COURSE"
+echo "Variables values of other script: $COURSE"
 echo "Process ID of other shell scripting: $$"
 
