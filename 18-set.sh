9b7db0056c11530075eb8a49f02bd3f92f79e4f0
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-26 19:29:28 -0400

    set -e command

---
+++ 18-set.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e 
+
+USERID=$(id -u)
+
+#(id -u) is a command and this output of command will given to "USERID" variable as a value
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please enter as run with super user"
+    exit 1 # manually exit if error occures
+else 
+    echo "You're super user"
+fi 
+
+dnf install mysqlll -y
+
+dnf install git -y
+
