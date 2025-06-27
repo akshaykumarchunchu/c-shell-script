97a09557732666fbebfcb5636c2b79abdfd22999
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-06-26 22:12:51 -0400

    delete logs

--- 19-delete-old-logs.sh
+++ 19-delete-old-logs.sh
@@ -7,7 +7,12 @@ if [ -d $SOURCE_DIRECTORY ]
 then 
     echo "source directory exists"
 else
-    echo "Please make source directory exists"
+    echo "Please make $SOURCE_DIRECTORY exists"
     exit 1
 fi
+
+FILES=(find $SOURCE_DIRECTORY -name ".log" -mtime +14)
+
+echo "files deletes: $FILES"
+
 
