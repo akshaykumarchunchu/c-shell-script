c860b81d0094732111835a374709cae67dc73d65
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-07-09 20:48:10 -0400

    diskspace

--- 20-diskspace.sh
+++ 20-diskspace.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-DISKSPACE=(df -hT|grep xfs)
+DISKSPACE=(df -hT | grep xfs)
+THERSHOLD=6
+
+if [ $DISKSPACE -gt $THERSHOLD]
+then 
+    usage=(df -hT | grep xfs | awk -F " " '{print$6F}')
+fi
 
 
 
