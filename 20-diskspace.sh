ffa650d8957b5850929a07c47515d5a852a9203f
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-07-10 20:47:24 -0400

    diskspace

--- 20-diskspace.sh
+++ 20-diskspace.sh
@@ -2,18 +2,18 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THERSHOLD=6
+MESSAGE=""
 
 while IFS= read -s $line
 do 
-    usage=$(echo $line | awk -F " " '{print$6F}')
+    usage=$(echo $line | awk -F " " '{print$6F}'| cut -d "%" -f1 )
     if [ $usage -ge $DISK_THERSHOLD ]
     then 
-        echo "$usage is more than $DISK_THERSHOLD"
-    else
-        echo "$usage is less then $DISK_THERSHOLD"
+        MESSAGE+=$usage is more than $DISK_THERSHOLD
     fi
 done <<< $DISK_USAGE    
 
+echo message: $MESSAGE
 
 #     if [ $DISKSPACE -gt $THERSHOLD ]
 # then 
