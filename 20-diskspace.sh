2bf5b4c84c42a8321e4eb3b308e6cda2e5ea0f6e
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-07-10 20:56:30 -0400

    diskspace

--- 20-diskspace.sh
+++ 20-diskspace.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THERSHOLD=6
 MESSAGE=""
 
-while IFS= read -s $line
+while IFS= read -r $line
 do 
     USAGE=$(echo $line | awk -F " " '{print$6F}'| cut -d "%" -f1 )
     if [ "$USAGE" -ge "$DISK_THERSHOLD" ]
