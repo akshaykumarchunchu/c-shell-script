986a365bc0351fb05d5b46aaa325d46001ba2495
akshaykumarchunchu <kumarchunchu.1994@gmail.com>
2025-07-10 21:06:12 -0400

    diskspace

--- 20-diskspace.sh
+++ 20-diskspace.sh
@@ -7,13 +7,14 @@ MESSAGE=""
 while IFS= read -r line
 do 
     USAGE=$(echo $line | awk -F " " '{print$6F}'| cut -d "%" -f1 )
+    FOLDER=$(echo $line | awk -F " " '{print$NF}')
     if [ "$USAGE" -ge "$DISK_THERSHOLD" ]
     then 
-        MESSAGE+="$usage is more than $DISK_THERSHOLD \n"
+        MESSAGE+="$FOLDER is more than $DISK_THERSHOLD, Current usage:$USAGE \n"
     fi
 done <<< $DISK_USAGE    
 
-echo message: $MESSAGE
+echo -e "message: $MESSAGE"
 
 #     if [ $DISKSPACE -gt $THERSHOLD ]
 # then 
